3e329ab6e59e91df418394a1ffd95a79fd87b883
Hà Chí Cao <92386482+Hachicao@users.noreply.github.com>
2025-05-09 09:54:44 +0700

    update background

--- frontend/app/standee/page.tsx
+++ frontend/app/standee/page.tsx
@@ -15,10 +15,10 @@ export default function StandeeDashboard() {
     <div className="relative min-h-screen">
       <BorderLines />
       <TopBar />
-      <section className="relative h-full flex justify-center ">
-        <BackgroundLayer />
+      {/* <section className="relative h-full flex justify-center "> */}
+        {/* <BackgroundLayer /> */}
       {/* <section className="relative h-full flex justify-center bg-[url(/img/BGSpring.svg)]"> */}
-      {/* <section className="relative h-full flex justify-center bg-gradient-to-b from-[#080077] via-[#65aff1] to-[#650bf7]"> */}
+      <section className="relative h-full flex justify-center bg-gradient-to-b from-[#080077] via-[#65aff1] to-[#650bf7]">
         <div className="container mx-auto px-6 pb-3 text-white">
           <GetRealWeather />
           <div className="grid bg-[#0f4da8] p-2 rounded-xl grid-cols-2 gap-2 mb-2">
